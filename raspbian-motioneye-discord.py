c9a4e623358a444784320cbbd54ec9d37a1d3163
umoderspace <55726262+umoderspace@users.noreply.github.com>
2019-12-28 06:46:51 +0100

    Create raspbian-motioneye-discord.py

---
+++ raspbian-motioneye-discord.py
@@ -0,0 +1,19 @@
+# raspberry-motioneye-discord by umoder.space
+# v1.0
+from discord_webhook import DiscordWebhook,DiscordEmbed
+import glob,os,datetime,urllib
+
+webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/CHANGE_THIS_WITH_YOUR_DISCORD_WEBHOOK_URL', username="DISCORDBOTNAME")
+
+# change foler to your Camera folder
+# will get the last .jpg from the current day
+folder = '/home/pi/motioneye/lib/Camera1/' + datetime.datetime.today().strftime('%Y-%m-%d')
+list_of_files = glob.glob(folder + '/*.jpg')
+latest_file = max(list_of_files, key=os.path.getctime)
+with open(latest_file, "rb") as f:
+    webhook.add_file(file=f.read(), filename=latest_file)
+
+embed = DiscordEmbed(title='MOTION DETECTION - ', description = 'timestamp : ' + datetime.datetime.today().strftime('%Y-%m-%d %Hh%mmn'), color=242424)
+
+webhook.add_embed(embed)
+response = webhook.execute()
